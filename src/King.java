188e4a96713852696ae7b3e6ac175f8bbc02b688
zippywin <eg264@hotmail.com>
2015-11-24 23:42:23 +1100

    Added stubs for King and Queen pieces

---
+++ src/King.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Zippy on 24/11/2015.
+ */
+public class King implements Piece {
+    private String location;
+
+    public King() {
+    }
+
+    @Override
+    public String getLocation() {
+        return null;
+    }
+
+    @Override
+    public String[] getValidMoves() {
+        return new String[0];
+    }
+}
---
+++ src/Queen.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Zippy on 24/11/2015.
+ */
+public class Queen implements Piece {
+    private String location;
+
+    public Queen() {
+
+    }
+
+    @Override
+    public String getLocation() {
+        return null;
+    }
+
+    @Override
+    public String[] getValidMoves() {
+        return new String[0];
+    }
+}
