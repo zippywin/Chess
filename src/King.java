a6d5211a3921d2882ac581394a3a642cd7ad16e4
zippywin <eg264@hotmail.com>
2015-11-25 15:11:49 +1100

    Updated comments on how Kings and Queens work

--- src/King.java
+++ src/King.java
@@ -1,15 +1,25 @@
 /**
  * Created by Zippy on 24/11/2015.
+ * - The King is a special piece in chess. It has the following special properties:
+ * - The King can move one space in any square surrounding it (including diagonals)
+ * - The King has the ability to castle (that comes with its own set of rules)
+ * - The King cannot move into check, making the move invalid.
+ * - If the King is in check, it must be put out of check on that turn.
+ * - If the King is in check, and it does not have any valid moves that put it out of check,
+ * it is checkmate and the game ends.
  */
 public class King implements Piece {
     private String location;
+    private Chessboard chessboard;
 
-    public King() {
+    public King(Chessboard chessboard, String location) {
+        this.location = location;
+        this.chessboard = chessboard;
     }
 
     @Override
     public String getLocation() {
-        return null;
+        return location;
     }
 
     @Override
--- src/Queen.java
+++ src/Queen.java
@@ -1,16 +1,20 @@
 /**
  * Created by Zippy on 24/11/2015.
+ * The Queen is a powerful piece in chess.
+ * - It can move in any of the eight directions for any number of spaces.
  */
 public class Queen implements Piece {
     private String location;
+    private Chessboard chessboard;
 
-    public Queen() {
-
+    public Queen(Chessboard chessboard, String location) {
+        this.location = location;
+        this.chessboard = chessboard;
     }
 
     @Override
     public String getLocation() {
-        return null;
+        return location;
     }
 
     @Override
